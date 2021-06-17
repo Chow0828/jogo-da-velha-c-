2e9d64de02786b4efdfc350ed661aa4f8e93658e
Chow0828 <74063033+Chow0828@users.noreply.github.com>
2021-06-17 20:32:56 -0300

    Add files via upload

---
+++ Jogo_Da_Velha/FrmPrincipal.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Jogo_Da_Velha
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.ForeColor = System.Drawing.Color.Black;
+            this.btn0.Location = new System.Drawing.Point(56, 25);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(79, 70);
+            this.btn0.TabIndex = 0;
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(167, 25);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(79, 70);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Location = new System.Drawing.Point(278, 25);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(79, 70);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Location = new System.Drawing.Point(56, 116);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(79, 70);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Location = new System.Drawing.Point(167, 116);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(79, 70);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.Black;
+            this.btn5.Location = new System.Drawing.Point(278, 116);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(79, 70);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.Color.Black;
+            this.btn6.Location = new System.Drawing.Point(56, 210);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(79, 70);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.Color.Black;
+            this.btn7.Location = new System.Drawing.Point(167, 210);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(79, 70);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.Color.Black;
+            this.btn8.Location = new System.Drawing.Point(278, 210);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(79, 70);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Font = new System.Drawing.Font("Humnst777 BlkCn BT", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 27);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "MENSAGEM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtMensagem.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtMensagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.ForeColor = System.Drawing.Color.Black;
+            this.txtMensagem.HideSelection = false;
+            this.txtMensagem.Location = new System.Drawing.Point(130, 311);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(253, 23);
+            this.txtMensagem.TabIndex = 10;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.LightGray;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Humnst777 BlkCn BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(115, 356);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(180, 39);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(-3, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(408, 295);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Location = new System.Drawing.Point(0, 292);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(405, 120);
+            this.panel2.TabIndex = 13;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(405, 407);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo_Da_Velha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Jogo_Da_Velha/FrmPrincipal.cs
@@ -0,0 +1,123 @@
+﻿   using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_Da_Velha
+{ 
+    public partial class FrmPrincipal : Form
+    {
+       
+        int rodadas = 0;
+
+        public void ChecarVencedor()
+        {
+            if(btn0.Text == "X" && btn1.Text =="X" && btn2.Text == "X" || btn3.Text == "X" && btn4.Text == "X" && btn5.Text == "X" || btn6.Text == "X" && btn7.Text == "X" && btn8.Text == "X" || btn0.Text == "X" && btn4.Text == "X" && btn8.Text == "X" || btn2.Text == "X" && btn4.Text == "X" && btn6.Text == "X" || btn0.Text == "X" && btn3.Text == "X" && btn6.Text == "X" || btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X" || btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X")
+            {
+                txtMensagem.Text = "Jogador 1 Venceu!";
+
+                if(txtMensagem.Text == "Jogador 1 Venceu!"  )
+                {
+                    txtMensagem.BackColor = Color.BlueViolet;
+                    txtMensagem.ForeColor = Color.White;
+
+                   //ideia caso fosse fazer o pontos 
+                  // int p;
+                 //  p =+ 1;
+                //   txtPontos.Text = p.ToString();
+
+                }
+            }
+            else if(btn0.Text == "O" && btn1.Text == "O" && btn2.Text == "O" || btn3.Text == "O" && btn4.Text == "O" && btn5.Text == "O" || btn6.Text == "O" && btn7.Text == "O" && btn8.Text == "O" || btn0.Text == "O" && btn4.Text == "O" && btn8.Text == "O" || btn2.Text == "O" && btn4.Text == "O" && btn6.Text == "O" || btn0.Text == "O" && btn3.Text == "O" && btn6.Text == "O" || btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O" || btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O")
+            {
+                txtMensagem.Text = "Jogador 2 Venceu!";
+
+                if (txtMensagem.Text == "Jogador 2 Venceu!")
+                {
+                    txtMensagem.BackColor = Color.Pink;
+                    txtMensagem.ForeColor = Color.Black;
+                    
+                }
+
+            }
+            else if(btn0.Text != "" && btn1.Text != ""&&  btn2.Text != "" && btn3.Text != "" && btn4.Text != "" && btn5.Text != "" && btn6.Text != "" && btn7.Text != "" && btn8.Text != "")
+            {
+                txtMensagem.Text = "Deu velha";
+            }
+           
+        }
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            //criei um objeto,assim quando eu clicar em um botão vai ser a partir deste como referência,
+            // com isso não precido programar em todos a mesma coisa
+
+            Button btn0 = (Button)sender;
+
+            //ele ja comeca com rodada=0,quando eu clico rodada=1,portanto o jogador 1=par|jogador2=Impar
+            if(btn0.Text == "")
+            {
+            //como rodadas esta iniciado em 0,quando for rodar vai clicar e rodadas será igual a 1,vez do jogador 2;
+
+                if (rodadas % 2 == 0)
+                {
+                    txtMensagem.Text = "Vez do Jogador 2!";
+                    btn0.Text = "X";
+                    ChecarVencedor();
+                    rodadas++;
+
+                }
+                else
+                {
+                    txtMensagem.Text = "Vez do Jogador 1!";
+                    btn0.Text = "O";
+                    ChecarVencedor();
+                    rodadas++;
+                }
+                
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+          
+                btn0.Text = "";
+                btn1.Text = "";
+                btn2.Text = "";
+                btn3.Text = "";
+                btn4.Text = "";
+                btn5.Text = "";
+                btn6.Text = "";
+                btn7.Text = "";
+                btn8.Text = "";
+
+            txtMensagem.Text = "";
+            txtMensagem.BackColor = Color.WhiteSmoke;
+
+
+            }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+    }
+    }
+
